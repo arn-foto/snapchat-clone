6e99a99ef8a1adeb1ac56a883949c185451466eb
Anthony N <foto.arn@gmail.com>
2021-01-12 00:50:24 -0800

    + setting up camera capture state

--- src/Js/Preview.js
+++ src/Js/Preview.js
@@ -1,8 +1,17 @@
 import React from "react";
+import { useSelector } from "react-redux";
+import { selectCameraImage } from "../features/cameraSlice";
 import "../styles/Preview.css";
 
 function Preview() {
-	return <div className="preview"></div>;
+	const cameraImage = useSelector(selectCameraImage);
+
+	return (
+		<div className="preview">
+			<img src={cameraImage} alt="" />
+			<h1>Preview, son</h1>
+		</div>
+	);
 }
 
 export default Preview;
