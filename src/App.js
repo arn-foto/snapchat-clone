75a6d0d5eca9cbc417bf3ddce931de98f2c64d33
Anthony N <foto.arn@gmail.com>
2021-01-11 17:37:30 -0800

    + Setting up webcam function

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from "react";
 import "./App.css";
+import WebcamCapture from "../src/WebcamCapture";
 
 function App() {
 	return (
 		<div className="App">
-			<h1>yo</h1>
+			<WebcamCapture />
 		</div>
 	);
 }
