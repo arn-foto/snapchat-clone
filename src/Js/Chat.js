5d232f8183900dbfcfbda9b7696989762306479c
Anthony N <foto.arn@gmail.com>
2021-01-12 19:56:44 -0800

    + Added react-timeAgo

--- src/Js/Chat.js
+++ src/Js/Chat.js
@@ -1,15 +1,17 @@
 import { Avatar } from "@material-ui/core";
+import StopRoundedIcon from "@material-ui/icons/StopRounded";
 import React from "react";
 import "../styles/Chat.css";
 
 function Chat({ id, username, timestamp, read, imageUrl, profilePic }) {
 	return (
 		<div className="chat">
-			<Avatar src={profilePic} />
+			<Avatar className="chat__avatar" src={profilePic} />
 			<div className="chat__info">
 				<h4>{username}</h4>
 				<p>Tap to view - {new Date(timestamp?.toDate()).toUTCString()}</p>
 			</div>
+			{!read && <StopRoundedIcon className="chat__readIcon" />}
 		</div>
 	);
 }
