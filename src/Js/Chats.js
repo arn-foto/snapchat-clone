ffa984f8aa1557e3948fb93542f8294174c14e60
Anthony N <foto.arn@gmail.com>
2021-01-12 19:06:07 -0800

    + connecting chat to firebase

--- src/Js/Chats.js
+++ src/Js/Chats.js
@@ -1,10 +1,26 @@
+import React, { useEffect, useState } from "react";
 import { Avatar } from "@material-ui/core";
 import SearchIcon from "@material-ui/icons/Search";
 import ChatBubbleIcon from "@material-ui/icons/ChatBubble";
-import React from "react";
 import "../styles/Chats.css";
+import { db } from "./firebase";
 
 function Chats() {
+	const [posts, setPosts] = useState([]);
+
+	useEffect(() => {
+		db.collection("posts")
+			.orderBy("timestamp", "desc")
+			.onSnapshot((snapshot) =>
+				setPosts(
+					snapshot.docs.map((doc) => ({
+						id: doc.id,
+						data: doc.data(),
+					}))
+				)
+			);
+	}, []);
+
 	return (
 		<div className="chats">
 			<div className="chats__header">
@@ -14,6 +30,23 @@ function Chats() {
 					<input placeholder="Friends" type="text" />
 				</div>
 				<ChatBubbleIcon className="chats__chatIcon" />
+			</div>
+			<div className="chat__posts">
+				{posts.map(
+					({
+						id,
+						data: { profilePic, username, timestamp, imageUrl, read },
+					}) => (
+						<Chat
+							key={id}
+							username={username}
+							timestamp={timestamp}
+							imageUrl={imageUrl}
+							read={read}
+							profilePic={profilePic}
+						/>
+					)
+				)}
 			</div>
 		</div>
 	);
